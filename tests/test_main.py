afbf22aa69b97f09994d20a254b28f79f9c825de
shahriar0999 <shahriar0999@proton.me>
2025-01-10 17:27:55 +0600

    add package into requirments file

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 import os
 from fastapi.testclient import TestClient
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.models import NLPDataInput, ImageDataInput
 from main import app
